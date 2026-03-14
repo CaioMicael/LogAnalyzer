df4fb784b39f2759fc70fa64596e5f8ed1ef5cdf
CaioMicael <contatocaiomicael@gmail.com>
2026-03-14 16:11:06 -0300

    feat: adicionado rabbitMQ.Client

---
+++ RabbitMQ/RabbitMQConsumer.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+
+namespace LogAnalyzer.RabbitMQ
+{
+    public class RabbitMQConsumer
+    {
+        public async Task Consume()
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(
+                queue: "log_queue",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+
+        }
+    }
+}
