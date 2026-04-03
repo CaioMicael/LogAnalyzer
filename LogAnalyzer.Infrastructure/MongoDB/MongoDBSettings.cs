9cb2bbe242df3257d3c6fc71ea0937e6685e76bb
Caio Micael <contatocaiomicael@gmail.com>
2026-04-03 01:06:50 -0300

    feat: adicionado dependencias MongoDB

---
+++ LogAnalyzer.Infrastructure/MongoDB/MongoDBSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace LogAnalyzer.Infrastructure.MongoDB
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+    }
+}
--- LogAnalyzer.Worker/Program.cs
+++ LogAnalyzer.Worker/Program.cs
@@ -2,15 +2,27 @@ using LogAnalyzer.Core.Interfaces;
 using LogAnalyzer.Core.RabbitMQ;
 using LogAnalyzer.Domain.Interfaces;
 using LogAnalyzer.Domain.Parsers;
+using LogAnalyzer.Infrastructure.MongoDB;
 using LogAnalyzer.Workers.Workers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.AddSingleton<IMessageConsumer, RabbitMQConsumer>();
 builder.Services.AddSingleton<ILogParser, LogParserResponseTime>();
 builder.Services.AddHostedService<MessageConsumerWorker>();
+
+builder.Services.Configure<MongoDbSettings>(
+    builder.Configuration.GetSection("MongoDB"));
+
+builder.Services.AddSingleton<IMongoClient>(sp =>
+{
+    var settings = sp.GetRequiredService<IOptions<MongoDbSettings>>().Value;
+    return new MongoClient(settings.ConnectionString);
+});
 
 using var host = builder.Build();
 
